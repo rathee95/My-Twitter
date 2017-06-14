87c6ebc7a6da60250c8d30316ce91d339c043be2
rathee95 <rathee95@gmail.com>
2017-06-14 23:45:51 +0530

    initial settings + started Tweet App

---
+++ tweets/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+
+class Tweet(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1)
+	content = models.CharField(max_length =140)
+	updated = models.DateTimeField(auto_now = True)
+	timestamp = models.DateTimeField(auto_now_add = True)
+
+
+	def __str__(self):
+		return str(self.content)
---
+++ tweets/urls.py
@@ -0,0 +1,8 @@
+from .views import tweet_detail_view , tweet_list_view
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^$', tweet_list_view,name="list"),
+    url(r'^1/$', tweet_detail_view,name="detail"),
+
+]
