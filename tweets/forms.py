31ec6cd9b44058cfa6cee27a653ac4c0f10381cd
rathee95 <rathee95@gmail.com>
2017-06-16 23:19:30 +0530

    added createview class for tweet

---
+++ tweets/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Tweet
+
+class TweetModelForm(forms.ModelForm):
+	class Meta:
+		model = Tweet
+		fields = ["content"]
+
+
--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,9 @@
-from .views import tweet_detail_view , tweet_list_view , TweetDetailView , TweetListView
+from .views import TweetDetailView , TweetListView , TweetCreateView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', TweetListView.as_view(),name="list"),
+	url(r'^$', TweetListView.as_view(),name="list"),
+    url(r'^create/$', TweetCreateView.as_view(),name="create"),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(),name="detail"), #pk --> dynamic url routing
 
 ]
