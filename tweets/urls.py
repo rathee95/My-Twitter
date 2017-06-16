3fa8bbd338419348db1763ad5a9c2174fa5bb1a8
rathee95 <rathee95@gmail.com>
2017-06-16 21:57:23 +0530

    Added class based views and their corresponding templates

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,8 @@
-from .views import tweet_detail_view , tweet_list_view
+from .views import tweet_detail_view , tweet_list_view , TweetDetailView , TweetListView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^$', tweet_list_view,name="list"),
-    url(r'^1/$', tweet_detail_view,name="detail"),
+    url(r'^$', TweetListView.as_view(),name="list"),
+    url(r'^1/$', TweetDetailView.as_view(),name="detail"),
 
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 from .models import Tweet
-
+from django.views.generic import DetailView , ListView
 # Create your views here.
 #CRUD List/Search
 
+
+class TweetDetailView(DetailView):
+	# template_name = "tweets/detail_view.html" #tweet_detail.html
+	queryset = Tweet.objects.all()
+	def get_object(self):
+		return Tweet.objects.get(id = 1)
+
+class TweetListView(ListView):
+	# template_name = "tweets/list_view.html" #tweet_list.html
+	queryset = Tweet.objects.all()
+
+#now how do the template gets "context" from the class based views?
+	# def get_context_data() -->see documentation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#function based views
 def tweet_detail_view(request,id = 1):
 	obj = Tweet.objects.get(id=id)
 	context = {"object": obj }
@@ -13,3 +45,4 @@ def tweet_list_view(request):
 	queryset = Tweet.objects.all()
 	context = {"object_list": queryset}
 	return render(request, "tweets/list_view.html", context)	
+##################################################################	
