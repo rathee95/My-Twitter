78341a543ca7ce7db1d406f11aab2deceffd42de
rathee95 <rathee95@gmail.com>
2017-06-19 16:32:02 +0530

    Modified bootdstrap - navbar

--- tweets/views.py
+++ tweets/views.py
@@ -76,14 +76,14 @@ class TweetDeleteView(LoginRequiredMixin,DeleteView):
 
 
 #################################################################
-#function based views
-def tweet_detail_view(request,id = 1):
-	obj = Tweet.objects.get(id=id)
-	context = {"object": obj }
-	return render(request, "tweets/detail_view.html", context)
+# #function based views
+# def tweet_detail_view(request,id = 1):
+# 	obj = Tweet.objects.get(id=id)
+# 	context = {"object": obj }
+# 	return render(request, "tweets/detail_view.html", context)
 
-def tweet_list_view(request):
-	queryset = Tweet.objects.all()
-	context = {"object_list": queryset}
-	return render(request, "tweets/list_view.html", context)	
+# def tweet_list_view(request):
+# 	queryset = Tweet.objects.all()
+# 	context = {"object_list": queryset}
+# 	return render(request, "tweets/list_view.html", context)	
 ##################################################################	
