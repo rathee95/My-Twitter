96624a28c87654e1c7dca6c7b75594eeae093d55
rathee95 <rathee95@gmail.com>
2017-06-24 18:55:31 +0530

    made tweetcreateAPI + ajax handling for a new tweet (important)

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,4 +1,4 @@
-from .views import TweetListAPIView
+from .views import TweetListAPIView, TweetCreateAPIView
  # TweetDetailView,TweetDeleteView ,  ,TweetUpdateView, TweetCreateView
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
@@ -8,6 +8,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
 	# url(r'^$', RedirectView.as_view(url="/")), #redirects to home 
 		url(r'^$', TweetListAPIView.as_view(),name = 'list'), #redirects to home 
+		url(r'^create/$', TweetCreateAPIView.as_view(),name = 'create'), #redirects to home 
 	# url(r'^search/$', TweetListView.as_view(),name="list"),
  #    url(r'^create/$', TweetCreateView.as_view(),name="create"),
  #    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(),name="detail"), #pk --> dynamic url routing
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,9 +1,26 @@
 from .serializers import TweetModelSerializer
 from rest_framework import generics
 from tweets.models import Tweet
+from django.db.models import Q
+from rest_framework import permissions
+
+
+class TweetCreateAPIView(generics.CreateAPIView):
+		serializer_class = TweetModelSerializer
+		permission_classes = [permissions.IsAuthenticated]
+
+		def perform_create(self,serializer):
+			serializer.save(user = self.request.user)
 
 class TweetListAPIView(generics.ListAPIView):
 
 	serializer_class = TweetModelSerializer
-	def get_queryset(self):
-		return Tweet.objects.all()
+	def get_queryset(self , *args , **kwargs):
+		qs = Tweet.objects.all()
+		query = self.request.GET.get("q",None)
+		if query is not None:
+			qs = qs.filter(
+				Q (content__icontains = query ) | 
+				Q (user__username__icontains = query )
+				)
+		return qs
