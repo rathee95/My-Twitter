a415aeefae9b8dd706b7c3acb693c4556891c96b
rathee95 <rathee95@gmail.com>
2017-06-27 18:08:34 +0530

    added profiles pages for users

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from .views import UserDetailView
+from django.conf.urls import url
+from django.views.generic.base import RedirectView
+
+
+
+urlpatterns = [
+	# url(r'^$', RedirectView.as_view(url="/")), #redirects to home 
+	# url(r'^search/$', TweetListView.as_view(),name="list"),
+ #    url(r'^create/$', TweetCreateView.as_view(),name="create"),
+    url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(),name="detail"), #pk --> dynamic url routing
+    # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(),name="update"), #pk --> dynamic url routing
+    # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(),name="delete"), 
+]
