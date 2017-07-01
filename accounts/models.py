510b5ec8b55eeee553e521393a9ffa87ce0b1594
rathee95 <rathee95@gmail.com>
2017-07-01 17:18:53 +0530

    added toggle for follow + signal for making user profile when user is created

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
-
+from django.urls import reverse_lazy
+from django.db.models.signals import post_save  
 # Create your models here.
 
 class UserProfileManager(models.Manager):
@@ -14,6 +15,25 @@ class UserProfileManager(models.Manager):
         except:
             pass
         return qs
+
+    def toggle_follow(self,user,to_toggle_user):
+        user_profile, created = UserProfile.objects.get_or_create(user = user)
+        if to_toggle_user in user_profile.following.all():
+            user_profile.following.remove(to_toggle_user)
+            added = False
+        else:
+            user_profile.following.add(to_toggle_user)
+            added = True
+
+        return added
+
+    def is_following(self,user,followed_by_user):
+        user_profile, created = UserProfile.objects.get_or_create(user = user)
+        if created:
+            return False
+        if followed_by_user in user_profile.following.all():
+            return True
+        return False
 
 class UserProfile(models.Model):
     user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile') # user.profile 
@@ -30,3 +50,18 @@ class UserProfile(models.Model):
     def get_following(self):
         users  = self.following.all() # User.objects.all().exclude(username=self.user.username)
         return users.exclude(username=self.user.username)
+
+    def get_follow_url(self):
+        return reverse_lazy("profiles:follow" , kwargs= {"username":self.user.username} )
+
+    def get_absolute_url(self):
+          return reverse_lazy("profiles:detail" , kwargs= {"username":self.user.username})  
+
+
+#deferred tasks run here
+def  post_save_user_receiver(sender,instance, created, *args, **kwargs):
+    if created:
+        new_profile = UserProfile.objects.get_or_create(user = instance)
+     
+
+post_save.connect(post_save_user_receiver , sender =settings.AUTH_USER_MODEL )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
+
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
@@ -7,8 +8,9 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
 	# url(r'^$', RedirectView.as_view(url="/")), #redirects to home 
 	# url(r'^search/$', TweetListView.as_view(),name="list"),
- #    url(r'^create/$', TweetCreateView.as_view(),name="create"),
+ #    url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(),name="detail"),url(r'^create/$', TweetCreateView.as_view(),name="create"),
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(),name="detail"), #pk --> dynamic url routing
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(),name="follow"),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(),name="update"), #pk --> dynamic url routing
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(),name="delete"), 
 ]
