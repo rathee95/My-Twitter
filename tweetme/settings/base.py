71e7b16cdca9606002d1f7eb415b4c87e15cd939
rathee95 <rathee95@gmail.com>
2017-07-13 21:34:55 +0530

    static_url

--- tweetme/settings/base.py
+++ tweetme/settings/base.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 CORS_REPLACE_HTTPS_REFERER      = False
 HOST_SCHEME                     = "http://"
